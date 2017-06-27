1d657e5c51abf9e3fc27d92f985291754b287852
ajboard13 <aaronjboard@live.com>
2017-06-27 11:52:16 -0400

    small tweak

--- src/Labs/Lab1.java
+++ src/Labs/Lab1.java
@@ -18,19 +18,19 @@ public class Lab1 {
 
         out.println("Welcome to Grand Circus' Room Detail Generator!");
 
-        do{
+        do {
             out.print("\nEnter Length: ");
             length = input.nextDouble();
             out.print("Enter Width: ");
             width = input.nextDouble();
             out.print("Enter height: ");
             height = input.nextDouble();
-            out.println("Area: " + length*width);
-            out.println("Perimeter: " + (length*2 + width*2));
-            out.println("Volume: " + length*width*height);
+            out.println("Area: " + length * width);
+            out.println("Perimeter: " + (length * 2 + width * 2));
+            out.println("Volume: " + length * width * height);
             out.print("\nContinue?(y/n) ");
             userCont = Objects.equals(input.next().toLowerCase(), "y");
-        }while (userCont);
+        } while (userCont);
 
         out.println("Goodbye!");
     }
