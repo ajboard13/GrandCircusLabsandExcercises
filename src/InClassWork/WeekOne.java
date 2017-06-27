c557a5134fbedbccb719ca52d415746e1ba598b5
ajboard13 <aaronjboard@live.com>
2017-06-27 15:34:00 -0400

    added more classwork. start lab 2

--- src/InClassWork/WeekOne.java
+++ src/InClassWork/WeekOne.java
@@ -8,23 +8,77 @@ import java.util.Scanner;
 
 public class WeekOne {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.print("Enter a age: ");
+//        String age = input.nextLine();
+//
+//        Integer.parseInt(age);
+//
+//        System.out.print("Enter an name: ");
+//        String name = input.nextLine();
+//
+//        System.out.println(name + " " + age);
+//
+//
+//        int newAge = 33;
+//        int newestAge;
+//        newestAge = 34;
+//        input.close();
 
-        System.out.print("Enter a age: ");
-        String age = input.nextLine();
 
-        Integer.parseInt(age);
+        //implicit casting example
 
-        System.out.print("Enter an name: ");
-        String name = input.nextLine();
+        double grade = 93;
 
-        System.out.println(name + " " + age);
+        System.out.println(grade);
+
+        double d = 95.0;
+        int i = 86;
+        int j = 91;
+
+        double average = (d+i+j) /3;
+        System.out.println(average);
 
 
-        int newAge = 33;
-        int newestAge;
-        newestAge = 34;
+        //explicit casting example
 
-        input.close();
+        int grade2 = (int) 93.75;
+        System.out.println(grade2);
+
+        if (grade2 > 100 || grade2 < 150){
+            System.out.println("you picked a number between 100 and 150");
+        }
+
+        String day = "Tuesday";
+
+        switch (day){
+            case "Monday":
+                System.out.println("Today is Monday");
+                break;
+            case "Tuesday":
+                System.out.println("Today is Tuesday");
+                break;
+            case "Wednesday":
+                System.out.println("Today is Wednesday");
+                break;
+            case "Thursday":
+                System.out.println("Today is Thursday");
+                break;
+            case "Friday":
+                System.out.println("Today is Friday");
+                break;
+            case "Saturday":
+                System.out.println("Today is Saturday");
+                break;
+            case "Sunday":
+                System.out.println("Today is Sunday");
+                break;
+            default:
+                System.out.println("No day selected");
+                break;
+        }
+
+
     }
 }
