60882c35401ff85f7eabe61187f47375afdda1c5
ajboard13 <aaronjboard@live.com>
2017-06-28 11:33:20 -0400

    added enhanced forloop examples

--- Tests/InClassTests/ForLoopExTests.java
+++ Tests/InClassTests/ForLoopExTests.java
@@ -9,4 +9,14 @@ public class ForLoopExTests {
     public void forLoopReturns1through4(){
         Assert.assertEquals(ForLoopEx.forLoop(), "0 1 2 3 4 ");
     }
+
+    @Test
+    public void forEachExTest(){
+        Assert.assertEquals(ForLoopEx.forEachLoopEx(), "10,20,30,40,50,");
+    }
+
+    @Test
+    public void forEachEx2Test(){
+        Assert.assertEquals(ForLoopEx.forEachLoopEx2(), "James,Larry,Tom,Lacy,");
+    }
 }
--- src/InClassWork/ForLoopEx.java
+++ src/InClassWork/ForLoopEx.java
@@ -3,12 +3,36 @@ package InClassWork;
 public class ForLoopEx {
     public static void main(String[] args) {
         forLoop();
+        forEachLoopEx();
+        forEachLoopEx2();
     }
 
-    public static String forLoop(){
+    public static String forLoop() {
         String str = "";
-        for (int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             str += i + " ";
+        }
+        System.out.println(str);
+        return str;
+    }
+
+    public static String forEachLoopEx() {
+        int[] numbers = {10, 20, 30, 40, 50};
+        String str = "";
+        for (int x : numbers) {
+            str += x + ",";
+
+        }
+        System.out.println(str);
+        return str;
+    }
+
+    public static String forEachLoopEx2() {
+        System.out.print("\n");
+        String str = "";
+        String[] names = {"James", "Larry", "Tom", "Lacy"};
+        for (String name : names) {
+            str += name + ",";
         }
         System.out.println(str);
         return str;
