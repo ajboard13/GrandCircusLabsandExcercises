76bb23036cbf5c1a268eb06144ffcd9242e1a84e
ajboard13 <aaronjboard@live.com>
2017-06-28 14:04:43 -0400

    fixed the output of lab 3 to hold any size integer

--- src/Labs/Lab3.java
+++ src/Labs/Lab3.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 public class Lab3 {
     private static Scanner input = new Scanner(System.in);
+    private static int userInt;
 
     public static void main(String[] args) {
-        int userInt;
         boolean userCont;
         System.out.println("Learn your squares and cubes!");
         do {
             userInt = checkForValidInt();
             if (userInt < 0) {
-                printPowerTablesForNegativeInts(userInt);
+                printPowerTablesForNegativeInts();
             } else if (userInt >= 0) {
-                printPowerTablesForPositiveInts(userInt);
+                printPowerTablesForPositiveInts();
             }
             System.out.println("\nWould you like to go again? (y/n)");
             userCont = input.next().equalsIgnoreCase("y");
@@ -24,23 +24,28 @@ public class Lab3 {
 
     }
 
-    private static void printPowerTablesForPositiveInts(int userInt) {
+    private static void printPowerTablesForPositiveInts() {
         printHeader();
         for (int i = 0; i <= userInt; i++) {
-            System.out.println(i + "\t\t\t" + (int) Math.pow(i, 2) + "\t\t\t" + (int) Math.pow(i, 3));
+            System.out.printf("\n%16d%16d%16d", i, (int) Math.pow(i, 2), (int) Math.pow(i, 3));
         }
     }
 
-    private static void printPowerTablesForNegativeInts(int userInt) {
+    private static void printPowerTablesForNegativeInts() {
         printHeader();
         for (int i = -1; i >= userInt; i--) {
-            System.out.println(i + "\t\t\t" + (int) Math.pow(i, 2) + "\t\t\t" + (int) Math.pow(i, 3));
+            System.out.printf("\n%16d%16d%16d", i, (int) Math.pow(i, 2), (int) Math.pow(i, 3));
         }
     }
 
     private static void printHeader() {
-        System.out.println("Number\t\tSquared\t\tCubed\n" +
-                "======\t\t=======\t\t=====");
+        System.out.printf("%16s%16s%16s\n%16s%16s%16s",
+                            "Number",
+                            "Squared",
+                            "Cubed",
+                            "======",
+                            "=======",
+                            "=====");
     }
 
     private static int checkForValidInt(){
