088accf249b6ed65ae4271e5080f035c51deee63
ajboard13 <aaronjboard@live.com>
2017-06-29 09:47:45 -0400

    tweaked person test class

--- src/BonusWork/PersonClasses/PersonTest.java
+++ src/BonusWork/PersonClasses/PersonTest.java
@@ -9,44 +9,39 @@ public class PersonTest {
     static Scanner input = new Scanner(System.in);
     static int userInt = 0;
     static String toStringResult;
+
     public static void main(String[] args) {
         boolean userCont;
 
         do {
+            toStringResult = "";
             checkForValidInt();
             chosePersonType();
             System.out.println(toStringResult);
             System.out.println("\nWould you like to go again? (y/n)");
             userCont = input.next().equalsIgnoreCase("y");
-        }while (userCont);
+        } while (userCont);
 
     }
 
-    private static int checkForValidInt(){
-        boolean validInt;
-        do {
-            System.out.println("which type of person are you? \n1 for Staff\n2 for student\n3 for ordinary person");
-            if (input.hasNextInt()) {
-                userInt = input.nextInt();
-                validInt = true;
-            } else {
-                System.out.println("not a valid choice!");
-                validInt = false;
-                input.nextLine();
-                input.nextLine();
-            }
-        } while (!validInt);
+    private static int checkForValidInt() {
+        System.out.println("which type of person are you? \n1 for Staff\n2 for student\n3 for ordinary person");
+        while (!input.hasNextInt()) {
+            System.out.println("not a valid choice! \nwhich type of person are you? \n1 for Staff\n2 for student\n3 for ordinary person ");
+            input.nextLine();
+        }
+        userInt = input.nextInt();
         return userInt;
     }
 
-    private static void chosePersonType(){
-        switch(userInt){
+    private static void chosePersonType() {
+        switch (userInt) {
             case 1:
                 Staff staff = new Staff("Aaron", "2736 drive", "college university", 1000000.00);
                 toStringResult = staff.toString();
                 break;
             case 2:
-                Student student = new Student("Aaron", "2345 street",1987,"Java", 10.00);
+                Student student = new Student("Aaron", "2345 street", 1987, "Java", 10.00);
                 toStringResult = student.toString();
                 break;
             case 3:
