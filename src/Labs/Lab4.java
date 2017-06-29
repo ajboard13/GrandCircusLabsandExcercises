6721e8453eb07c04de4351f7fe547201a8984105
ajboard13 <aaronjboard@live.com>
2017-06-29 15:27:38 -0400

    extracted a method from the main method in Lab 4

--- src/Labs/Lab4.java
+++ src/Labs/Lab4.java
@@ -10,14 +10,9 @@ public class Lab4 {
     private static InputValidator inputValidator = new InputValidator();
 
     public static void main(String[] args) {
-        long factorialResult;
-        int minNum = 0;
-        int maxNum = 20;
         System.out.println("Welcome to the Factorial Calculator!\n");
         do {
-            int userInt = inputValidator.getValidIntBetweenTwoNumbers(minNum, maxNum);
-            factorialResult = calculateFactorial(userInt);
-            System.out.println("The factorial of " + userInt + " is " + factorialResult);
+            runFactorialCalculator();
             inputValidator.checkForUserContinue();
         } while (inputValidator.isUserCont());
         System.out.println("Goodbye!");
@@ -29,6 +24,15 @@ public class Lab4 {
             return 1;
         result = calculateFactorial(n-1) * n;
         return result;
+    }
+
+    private static void runFactorialCalculator(){
+        int minNum = 0;
+        int maxNum = 20;
+        int userInt = inputValidator.getValidIntBetweenTwoNumbers(minNum, maxNum);
+        long factorialResult = calculateFactorial(userInt);
+        System.out.println("The factorial of " + userInt + " is " + factorialResult);
+
     }
 
 
