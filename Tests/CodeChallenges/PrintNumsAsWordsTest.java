4462a59913d8fb0df1d86478737f158d41e1699d
ajboard13 <aaronjboard@live.com>
2017-07-28 19:04:12 -0400

    finished an app that converts numbers two words

--- Tests/CodeChallenges/PrintNumsAsWordsTest.java
+++ Tests/CodeChallenges/PrintNumsAsWordsTest.java
@@ -8,53 +8,109 @@ import static org.junit.Assert.*;
  * Created by user on 7/28/2017.
  */
 public class PrintNumsAsWordsTest {
+        PrintNumsAsWords pn = new PrintNumsAsWords();
+
         @Test
         public void Test1() {
             String expected = "one";
-            String actual = PrintNumsAsWords.answer(1);
+            String actual = pn.answer(1);
             assertEquals(expected, actual);
         }
         @Test
         public void Test2() {
             String expected = "two";
-            String actual = PrintNumsAsWords.answer(2);
+            String actual = pn.answer(2);
             assertEquals(expected, actual);
         }
         @Test
         public void Test3() {
             String expected = "nine";
-            String actual = PrintNumsAsWords.answer(9);
+            String actual = pn.answer(9);
             assertEquals(expected, actual);
         }
         @Test
         public void Test4() {
             String expected = "ten";
-            String actual = PrintNumsAsWords.answer(10);
+            String actual = pn.answer(10);
             assertEquals(expected, actual);
         }
         @Test
         public void Test5() {
             String expected = "twenty";
-            String actual = PrintNumsAsWords.answer(20);
+            String actual = pn.answer(20);
             assertEquals(expected, actual);
         }
         @Test
         public void Test6() {
             String expected = "thirteen";
-            String actual = PrintNumsAsWords.answer(13);
+            String actual = pn.answer(13);
             assertEquals(expected, actual);
         }
         @Test
         public void Test7() {
             String expected = "fifteen";
-            String actual = PrintNumsAsWords.answer(15);
+            String actual = pn.answer(15);
             assertEquals(expected, actual);
         }
         @Test
         public void Test21() {
-            String expected = "twentyone";
-            String actual = PrintNumsAsWords.answer(21);
+            String expected = "twenty-one";
+            String actual = pn.answer(21);
             assertEquals(expected, actual);
         }
 
+    @Test
+    public void Test97() {
+        String expected = "ninety-seven";
+        String actual = pn.answer(97);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void Test122() {
+        String expected = "one hundred twenty-two";
+        String actual = pn.answer(122);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Test974() {
+        String expected = "nine hundred seventy-four";
+        String actual = pn.answer(974);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Test1325() {
+        String expected = "one thousand, three hundred twenty-five";
+        String actual = pn.answer(1325);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Test206489() {
+        String expected = "two hundred six thousand, four hundred eighty-nine";
+        String actual = pn.answer(206489);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Test45678() {
+        String expected = "forty-five thousand, six hundred seventy-eight";
+        String actual = pn.answer(45678);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Test206() {
+        String expected = "two hundred six";
+        String actual = pn.answer(206);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Test45678490() {
+        String expected = "forty-five million, six hundred seventy-eight thousand, four hundred ninety";
+        String actual = pn.answer(45678490);
+        assertEquals(expected, actual);
+    }
 }
--- src/CodeChallenges/PrintNumsAsWords.java
+++ src/CodeChallenges/PrintNumsAsWords.java
@@ -1,63 +1,62 @@
 package CodeChallenges;
 
-/**
- * Created by user on 7/28/2017.
+/*
+Aaron Board
  */
 public class PrintNumsAsWords {
-    static boolean isTeen = false;
+    private static String[] lowNums = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+            "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+    private static final String[] tens = {
+            "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+
+    private static final String[] prefixes = {
+            "thousand", "million", "billion"};
 
     public static String answer(int num) {
-        int temp = num;
-        String result = "";
-        for (int i = 0; i < String.valueOf(num).length(); i++) {
-            result += findNum(temp);
-            if (String.valueOf(num).length() == 1 && !isTeen) {
-                result += findOnes(temp);
-            } else {
-                temp = temp % (int) Math.pow(10, (int) Math.log10(temp));
-            }
+        if (num <= 999) {
+            return convertLargeNum(num);
         }
-        return result;
-    }
-
-    private static int takeOffFirstNum(int num) {
-        String temp = String.valueOf(num);
-        temp = temp.substring(1);
-        int revisedInt = Integer.parseInt(temp);
-        return revisedInt;
-    }
-
-    private static String findNum(int num) {
-        if (String.valueOf(num).length() == 1) {
-            return findOnes(num);
-        } else if (String.valueOf(num).length() == 2) {
-            if (num < 20 && num > 10) {
-                return findTeens(num);
-            } else {
-                return findTens(num);
+        String s = null;
+        int t = 0;
+        while (num > 0) {
+            if (num % 1000 != 0) {
+                String s2 = convertLargeNum(num % 1000);
+                if (t > 0) {
+                    s2 = s2 + " " + prefixes[t - 1];
+                }
+                if (s == null) {
+                    s = s2;
+                } else {
+                    s = s2 + ", " + s;
+                }
             }
+            num /= 1000;
+            t++;
+        }
+        return s;
+    }
+
+    private static String convertLargeNum(int num) {
+        String s1 = lowNums[num / 100] + " hundred";
+        String s2 = convertLowNums(num % 100);
+        if (num <= 99) {
+            return s2;
+        } else if (num % 100 == 0) {
+            return s1;
         } else {
-            return "";
+            return s1 + " " + s2;
         }
     }
 
-    private static String findTeens(int num) {
-        String[] teens = {"eleven", "twelve", "thirteen", "fourteen",
-                "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
-        isTeen = true;
-        return teens[(num % 10) - 1];
-    }
-
-    private static String findTens(int num) {
-        String[] tens = {"ten", "twenty", "thirty", "forty", "fifty",
-                "sixty", "seventy", "eighty", "ninety"};
-        return tens[(num / 10) - 1];
-    }
-
-    private static String findOnes(int num) {
-        String[] ones = {"one", "two", "three", "four",
-                "five", "six", "seven", "eight", "nine"};
-        return ones[num - 1];
+    private static String convertLowNums(int num) {
+        if (num < 20) {
+            return lowNums[num];
+        }
+        String s = tens[num / 10 - 2];
+        if (num % 10 == 0) {
+            return s;
+        }
+        return s + "-" + lowNums[num % 10];
     }
 
 }
